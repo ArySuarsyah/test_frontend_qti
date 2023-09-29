9ea7dd04332d2ae5dd75cc8cd6c905948dd19a2c
ArySuarsyah <gotrany97@gmail.com>
2023-09-29 13:51:19 +0800

    add header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BiMenu } from "react-icons/bi";
+
 import { SlSpeedometer } from "react-icons/sl";
 import {
   AiOutlineLineChart,
@@ -8,6 +8,7 @@ import {
   AiOutlineDown,
 } from "react-icons/ai";
 import Image from "next/image";
+import Header from "./Header";
 
 export default function Layout({ children, token: userToken }) {
   const [usrManagement, setUsrManagement] = useState(false);
@@ -16,29 +17,9 @@ export default function Layout({ children, token: userToken }) {
     setUsrManagement(!usrManagement);
   };
   return (
-    <div className="bg-white">
-      <div className="py-3 px-10">
-        <div className="flex justify-between">
-          <div className="flex items-center gap-3">
-            <BiMenu size={35} />
-            <span className="font-bold">LOGO</span>
-          </div>
-          <div className="flex gap-5 items-center">
-            <div className="bg-[#E3645C] flex w-12 h-12 rounded-full justify-center items-center">
-              <span className="font-semibold text-white">RA</span>
-              {/* <Image /> */}
-            </div>
-            <div className="flex flex-col">
-              <span className="font-semibold">Riana Andra</span>
-              <span className="text-sm text-slate-400">Administrator</span>
-            </div>
-            <div>
-              <AiOutlineDown size={20} />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-5">
+    <div className="bg-[#f9f9f9] h-screen">
+      <Header/>
+      <div className="flex gap-3">
         <aside className="w-96">
           <div className="p-5 flex flex-col gap-5">
             <span className="font-bold text-[#66666675]">Analysis</span>
